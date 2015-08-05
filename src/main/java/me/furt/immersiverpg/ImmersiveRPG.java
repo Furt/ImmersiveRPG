18198e47f02ae1edb3c48c859e6300ed573145fa
Terry Earnheart <lit_69@hotmail.com>
2015-08-05 07:52:46 -0500

    start of working with events.

--- src/main/java/me/furt/immersiverpg/ImmersiveRPG.java
+++ src/main/java/me/furt/immersiverpg/ImmersiveRPG.java
@@ -1,22 +1,25 @@
-/**
- * ImmersiveRPG
- * Created by Furt on 8/4/2015.
- */
 package me.furt.immersiverpg;
 
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import me.furt.immersiverpg.events.TestEvents;
+import net.minecraftforge.common.MinecraftForge;
 
+/**
+ * ImmersiveRPG
+ * Created by Furt on 8/4/2015.
+ */
 @Mod(modid = ImmersiveRPG.MODID, version = ImmersiveRPG.VERSION)
 public class ImmersiveRPG
 {
     public static final String MODID = "ImmersiveRPG";
     public static final String VERSION = "1.7.10-0.0.1";
 
-    @Mod.Instance("ImmersiveRPG")
+    @Instance("ImmersiveRPG")
     public static ImmersiveRPG instance;
 
     @EventHandler
@@ -29,6 +32,7 @@ public class ImmersiveRPG
     public void init(FMLInitializationEvent event)
     {
 		// gui, tileentity, recipes, etc
+        MinecraftForge.EVENT_BUS.register(new TestEvents());
     }
 
     @EventHandler
---
+++ src/main/java/me/furt/immersiverpg/events/TestEvents.java
@@ -0,0 +1,20 @@
+package me.furt.immersiverpg.events;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraft.util.ChatComponentText;
+
+/**
+ * ImmersiveRPG
+ * Created by Furt on 8/5/2015.
+ */
+public class TestEvents
+{
+    @SubscribeEvent
+    public void loginMessage(PlayerLoggedInEvent event)
+    {
+        //event.player
+        //if(!event.player.getEntityWorld().isRemote)
+        event.player.addChatComponentMessage(new ChatComponentText("Hello " + event.player.getDisplayName() + ", have a nice day!"));
+    }
+}
