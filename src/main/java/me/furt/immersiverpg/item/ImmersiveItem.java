319a4e4ea53df45c32fcf8ff06dc6b182355ef7d
Terry Earnheart <lit_69@hotmail.com>
2015-08-08 23:16:09 -0500

    fixing repo settings

---
+++ src/main/java/me/furt/immersiverpg/item/ImmersiveItem.java
@@ -0,0 +1,15 @@
+package me.furt.immersiverpg.item;
+
+import net.minecraft.item.Item;
+
+/**
+ * ImmersiveRPG
+ * Created by Furt on 8/8/2015.
+ */
+public class ImmersiveItem extends Item {
+
+    public ImmersiveItem()
+    {
+
+    }
+}
---
+++ src/main/java/me/furt/immersiverpg/item/ItemManager.java
@@ -0,0 +1,13 @@
+package me.furt.immersiverpg.item;
+
+/**
+ * ImmersiveRPG
+ * Created by Furt on 8/8/2015.
+ */
+public class ItemManager {
+
+    public static final void init()
+    {
+
+    }
+}
